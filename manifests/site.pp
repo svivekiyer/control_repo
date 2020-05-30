8b2675fa5e9d33bf670507ff59d3c81a90c81bf2
svivekiyer <svivekiyer@outlook.com>
2020-05-30 19:10:50 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'This is a readme file part of Learning Activity',
-    owner   => 'root',
-  }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
